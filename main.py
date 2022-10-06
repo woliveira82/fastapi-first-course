f77e1dfb4a1ced2d5d03aa825e76253dd2297104
Weiner Oliveira <weiner.oliveira@zharta.com>
2022-10-06 16:22:54 +0000

    feat: add parameters definition

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'status': 'ok'}
+
+
+@app.get('/blog/unpublished')
+def get_unpublished_blog():
+    return {'data': [1, 2]}
+
+
+@app.get('/blog/{id}')
+def get_blog(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def get_blog_comments(id: int):
+    return {'data': [1, 2]}
