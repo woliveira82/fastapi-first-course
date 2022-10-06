452935db486ac7daeb0a7a8e87c0fe7f95c9654a
Weiner Oliveira <weiner.oliveira@zharta.com>
2022-10-06 16:54:00 +0000

    feat: add query parameters

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'status': 'ok'}
+@app.get('/blogs')
+def list_all_blogs(sort, published: bool = True, limit: int = 10):
+    phrase = f'{limit} blogs from database' if published else f'{limit} published blogs from database'
+    return {'data': [phrase]}
 
 
-@app.get('/blog/unpublished')
-def get_unpublished_blog():
+@app.get('/blogs/unpublished')
+def get_unpublished_blogs():
     return {'data': [1, 2]}
 
 
-@app.get('/blog/{id}')
-def get_blog(id: int):
+@app.get('/blogs/{id}')
+def get_blog_by_id(id: int):
     return {'data': id}
 
 
-@app.get('/blog/{id}/comments')
-def get_blog_comments(id: int):
+@app.get('/blogs/{id}/comments')
+def list_comments_from_blog_id(id: int, limit: int = 10):
     return {'data': [1, 2]}
